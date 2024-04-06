23b636f2197637e1e303f4787a275b6edc2a835a
TC Wang <wangtct@hotmail.com>
2024-04-06 15:50:08 +0800

    market

---
+++ components/charts/Market01.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="m-4">
+    <h2>A7建案熱門排行榜 看板</h2>
+    <Bar id="myChart" :options="chartOptions" :data="chartData"></Bar>
+  </div>
+</template>
+
+<script>
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+export default {
+  name: "BarChartTeamplate",
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 63, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 63, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+        indexAxis: "x",
+        scales: {
+          y: {
+            beginAtZero: false,
+          },
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style scoped></style>
--- pages/market/index.vue
+++ pages/market/index.vue
@@ -3,10 +3,8 @@
     <h1>城市探索 - A7重劃區 建案房屋市場行情</h1>
     <MarketTable />
     <h1 class="mt-4">房屋市場分析看板</h1>
-    <MarketAnalysis />
 
-    <h1>Chart Test</h1>
-    <ChartsBar01 />
+    <ChartsMarket01 />
   </div>
 </template>
 
