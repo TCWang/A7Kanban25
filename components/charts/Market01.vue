6836f506709737a54859cb920c525ab885ce19b8
TC Wang <wangtct@hotmail.com>
2024-04-07 11:16:31 +0800

    chart

--- components/charts/Market01.vue
+++ components/charts/Market01.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-4">
-    <h2>A7建案熱門排行榜 看板</h2>
+    <h2>A7 房屋市場趨勢 看板 2023 Q1 - 2024 Q1</h2>
     <Bar id="myChart" :options="chartOptions" :data="chartData"></Bar>
   </div>
 </template>
@@ -15,6 +15,8 @@ import {
   BarElement,
   CategoryScale,
   LinearScale,
+  LineElement,
+  PointElement,
 } from "chart.js";
 
 ChartJS.register(
@@ -23,37 +25,36 @@ ChartJS.register(
   Legend,
   BarElement,
   CategoryScale,
-  LinearScale
+  LinearScale,
+  LineElement,
+  PointElement
 );
 
 export default {
-  name: "BarChartTeamplate",
+  name: "MixBarChartTeamplate",
   components: { Bar },
   data() {
     return {
       chartData: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["2023 Q1", "2023 Q2", "2023 Q3", "2023 Q4", "2024 Q1"],
         datasets: [
           {
-            label: "# of votes",
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 63, 0.2)",
-            ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 63, 1)",
-            ],
+            yAxisID: "A",
+            label: "銷售戶數",
+            data: [301, 697, 999, 859, 399],
+            backgroundColor: ["rgba(54, 162, 235, 0.2)"],
+            borderColor: ["rgba(54, 162, 235, 1)"],
             borderWidth: 1,
+            type: "bar",
+          },
+          {
+            yAxisID: "B",
+            label: "平均每坪單價(萬元)",
+            data: [37.5, 37.4, 38.7, 40.0, 41.6],
+            backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+            borderColor: ["rgba(255, 99, 132, 1)"],
+            borderWidth: 1,
+            type: "line",
           },
         ],
       },
@@ -61,8 +62,18 @@ export default {
         responsive: true,
         indexAxis: "x",
         scales: {
-          y: {
-            beginAtZero: false,
+          A: {
+            type: "linear",
+            position: "left",
+            ticks: { beginAtZero: true, color: "blue" },
+            // Hide grid lines, otherwise you have separate grid lines for the 2 y axes
+            grid: { display: false },
+          },
+          B: {
+            type: "linear",
+            position: "right",
+            ticks: { color: "red", min: 30, max: 45 },
+            grid: { display: false },
           },
         },
       },
