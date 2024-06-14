9e53318a534508a98c51248aa96479b1654976d2
TC Wang <wangtct@hotmail.com>
2024-06-14 17:44:19 +0800

    two windows

--- pages/market/index.vue
+++ pages/market/index.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="container">
-    <h1>地區建案 - 實價資訊</h1>
-
-    <!-- <MarketTableUip />
-    <br />
-    <MarketTableUiq /> -->
-
-    <MarketTablePrice />
-    <br />
-    <MarketTableQuantity />
+  <div class="container flex flex-col h-screen">
+    <h2>地區建案 - 實價資訊</h2>
+    <div class="flex-1 h-1/2 w-full p-4 bg-gray-200 overflow-y-scroll">
+      <!-- 上方窗口 -->
+      <MarketTablePrice />
+    </div>
+    <div class="flex-1 h-1/2 w-full p-4 bg-gray-300 overflow-y-scroll">
+      <!-- 下方窗口 -->
+      <MarketTableQuantity />
+    </div>
   </div>
 </template>
 
@@ -17,3 +17,23 @@ useHead({
   title: "地區建案 - 實價資訊",
 });
 </script>
+
+<style scoped>
+/* 如果需要其他樣式，可以在這裡添加 */
+.content {
+  /* 為內容容器設置固定高度，使其超出高度時顯示滾動條 */
+  max-height: 100%;
+  /* 設置滾動條樣式 */
+  scrollbar-width: thin;
+  scrollbar-color: #4a5568 transparent; /* 滾動條顏色和背景色 */
+  /* 添加滾動條樣式 */
+  /* 非webkit瀏覽器 */
+  scrollbar-track-color: transparent; /* 滾動條軌道顏色 */
+  scrollbar-face-color: #4a5568; /* 滾動條輪廓顏色 */
+  scrollbar-arrow-color: #cbd5e0; /* 滾動條箭頭顏色 */
+  scrollbar-highlight-color: #cbd5e0; /* 滾動條高亮顏色 */
+  scrollbar-darkshadow-color: #cbd5e0; /* 滾動條陰影顏色 */
+  /* webkit瀏覽器 */
+  scrollbar-color: #4a5568 transparent;
+}
+</style>
