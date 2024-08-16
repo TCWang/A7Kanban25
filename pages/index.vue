84e1604c49d6ac1628ea24f0c32f69532e3cd045
TC Wang <wangtct@hotmail.com>
2024-08-17 01:03:23 +0800

    index

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="">
     <h1>A7新林口-社區網站: 15.城市探索資訊平台</h1>
-    <NuxtImg
-      src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/logo/LOG-01-map.png"
-      class="w-full"
-    />
+    <NuxtImg src="/images/introduction/LOG-01-map.png" class="w-full" />
   </div>
 </template>
 
